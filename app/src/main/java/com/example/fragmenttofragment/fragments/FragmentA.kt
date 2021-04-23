d130cdd2d6278914c09442831071fac14c0e728b
sooryanarayanan <sooryanarayanan@qburst.com>
2021-04-23 19:28:32 +0530

    Added datepicker

--- app/src/main/java/com/example/fragmenttofragment/fragments/FragmentA.kt
+++ app/src/main/java/com/example/fragmenttofragment/fragments/FragmentA.kt
@@ -1,13 +1,18 @@
 package com.example.fragmenttofragment.fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.Toast
 import com.example.fragmenttofragment.Communicator
+import com.example.fragmenttofragment.MainActivity
 import com.example.fragmenttofragment.R
 import com.example.fragmenttofragment.databinding.FragmentABinding
+import java.util.*
 
 
 class FragmentA : Fragment() {
@@ -15,6 +20,7 @@ class FragmentA : Fragment() {
     lateinit var communicator: Communicator
     private var _binding: FragmentABinding? = null
     private val binding get() = _binding!!
+
 
 
 
@@ -28,8 +34,27 @@ class FragmentA : Fragment() {
         communicator = activity as Communicator
         binding.btnSend.setOnClickListener { communicator.passData(binding.inputMessage.text.toString()) }
 
+        val context = context as MainActivity
+        
+        val calender = Calendar.getInstance()
+        val year = calender.get(Calendar.YEAR)
+        val month = calender.get(Calendar.MONTH)
+        val day = calender.get(Calendar.DAY_OF_MONTH)
+
+        binding.inputMessage.setOnClickListener {
+            val datePicker =
+                DatePickerDialog(context, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                binding.inputMessage.text = "$dayOfMonth"
+                }, year, month, day)
+                
+                datePicker.show()
+        }
+
         return view
     }
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
